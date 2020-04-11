45f285b2f33d304ac041ba9e19de247f7376dd39
gavenhutchens <gavenhutchens@gmail.com>
2020-04-11 14:05:40 -0500

    formatting using atom-beautify

--- app.js
+++ app.js
@@ -8,74 +8,74 @@ var isCurrentLive = false;
 
 const options = {
 
-	//57199432 = sam user id
-	//rachel user id = 241165842
-	//imaqtpie user id = 24991333 
-	url: 'https://api.twitch.tv/helix/streams/?user_id=241165842',
-	method: 'GET',
-	headers: {
-	  'Client-ID': config.twitchid
-	}
-}; 
+  //57199432 = sam user id
+  //rachel user id = 241165842
+  //imaqtpie user id = 24991333
+  url: 'https://api.twitch.tv/helix/streams/?user_id=241165842',
+  method: 'GET',
+  headers: {
+    'Client-ID': config.twitchid
+  }
+};
 
 function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
 async function polltwitchapi() {
 
 
-	request(options, function(err, res, body) {
-		
-		var jsonObject = JSON.parse(body);
-		console.log(jsonObject);
-		console.log("current timestamp in twitch request function is:");
-		var date = new Date(Date.now());
-		console.log(date.toString());
+  request(options, function(err, res, body) {
 
-		try {
-		streamtype = jsonObject.data[0].type;
-		console.log("Streamtype is: " + streamtype);
+    var jsonObject = JSON.parse(body);
+    console.log(jsonObject);
+    console.log("current timestamp in twitch request function is:");
+    var date = new Date(Date.now());
+    console.log(date.toString());
 
-		streamurl = jsonObject.data[0].user_name;
-		console.log("StreamURL is: " + "https://twitch.tv/" + streamurl);
+    try {
+      streamtype = jsonObject.data[0].type;
+      console.log("Streamtype is: " + streamtype);
 
-		streamurl = "https://twitch.tv/" + streamurl;
+      streamurl = jsonObject.data[0].user_name;
+      console.log("StreamURL is: " + "https://twitch.tv/" + streamurl);
 
-		//client.channels.get(config.channelid).send(streamurl);
+      streamurl = "https://twitch.tv/" + streamurl;
 
-		} catch (error) {
+      //client.channels.get(config.channelid).send(streamurl);
 
-		console.log("catching error, channel not live");
-		};
+    } catch (error) {
 
-	}); 
+      console.log("catching error, channel not live");
+    };
 
-
-	if(streamtype === "live" && isCurrentLive === false) {
-	console.log("inside live if");
-	client.channels.get(config.channelid).send(streamurl);
-	client.channels.get(config.channelid).send("test mail you are live <@653678313822486551>");
-	console.log("sent live mail to disc");	
-	isCurrentLive = true;
-  	}
- 
-	if(streamtype === "false") {
-	console.log("inside false if");
-	isCurrentLive = false;
-	}
+  });
 
 
-	await sleep(10000); 
-	polltwitchapi(); //recurse
+  if (streamtype === "live" && isCurrentLive === false) {
+    console.log("inside live if");
+    client.channels.get(config.channelid).send(streamurl);
+    client.channels.get(config.channelid).send("test mail you are live <@653678313822486551>");
+    console.log("sent live mail to disc");
+    isCurrentLive = true;
+  }
+
+  if (streamtype === "false") {
+    console.log("inside false if");
+    isCurrentLive = false;
+  }
+
+
+  await sleep(10000);
+  polltwitchapi(); //recurse
 }
 
 
 
 
 client.on("ready", () => {
-  console.log("RachelBot is online!"); 
+  console.log("RachelBot is online!");
   polltwitchapi();
 
 });
@@ -87,9 +87,9 @@ client.on("message", (message) => {
   //shift removes one element from the args array and returns it
   const command = args.shift().toLowerCase();
 
-  
+
   if (!message.content.startsWith(config.prefix) || message.author.bot) {
-   return;
+    return;
   }
 
   if (command === "howtocarrygameswhenimfeeding") {
@@ -98,19 +98,19 @@ client.on("message", (message) => {
 
   if (command === "help") {
     message.channel.send("Commands are as follows:\n!help\n!join\n" +
-    "!leave\n");
+      "!leave\n");
   }
 
-  if(command === "join") {
-    message.member.voiceChannel.join();	
+  if (command === "join") {
+    message.member.voiceChannel.join();
   }
 
-  if(command === "leave") {
+  if (command === "leave") {
     message.member.voiceChannel.leave();
   }
 
-  if(command === "stream") {
-	  message.channel.send("https://twitch.tv/rachelae");
+  if (command === "stream") {
+    message.channel.send("https://twitch.tv/rachelae");
   }
 
 });
@@ -118,7 +118,7 @@ client.on("message", (message) => {
 client.on("guildMemberAdd", (member) => {
 
   member.send("Welcome to Green Hills! Send Gaven a message if you need anything." +
-  " Be sure to check out gavenhutchens.com for more info about my creator!");
+    " Be sure to check out gavenhutchens.com for more info about my creator!");
 });
 
 
